2d5f52268de86463970b57308b6ac05e9561e651
Cristian Ofi <cristian.munoz@iti.edu.ec>
2022-07-21 18:53:54 -0500

    html-php

--- 04-semana/loops.php
+++ 04-semana/loops.php
@@ -1,30 +1,35 @@
 <?php
 
-//whilw
 
-    //Ejercicio
-    // $a = 0;
-    // while ($a <= 10) {
-    //     $res = 4 * $a;
-    //     echo("La multiplacionde 4 * $a es $res\n");
-    //     $a++;
-    // }
+// $zorritos = ["https://randomfox.ca/images/1.jpg", "https://randomfox.ca/images/2.jpg", "https://randomfox.ca/images/3.jpg"];
 
-//do while
+// //whilw
+
+//     //Ejercicio
+//     // $a = 0;
+//     // while ($a <= 10) {
+//     //     $res = 4 * $a;
+//     //     echo("La multiplacionde 4 * $a es $res\n");
+//     //     $a++;
+//     // }
+
+// //do while
 $frutas = ['pera', 'manzana', 'mandarina'];
 
-do {
+// do {
     
-    $fr = readline("⌚  Ingrese una fruta ");
+//     $fr = readline("⌚  Ingrese una fruta ");
   
 
-} while (in_array($fr,$frutas));
-array_push($frutas,$fr);
-print_r($frutas);
-//for
-for ($i=0; $i < count(); $i++) { 
-    # code...
-}
+// } while (in_array($fr,$frutas));
+// array_push($frutas,$fr);
+// print_r($frutas);
+// //for
+// for ($i=0; $i < count(); $i++) { 
+//     # code...
+// }
 //forEach
 
 //Funciones
+
+print_r($_REQUEST);
