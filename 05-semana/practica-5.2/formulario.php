1d086af9b78d3035c51fd57a0571ed6d05dc67b1
Cristian Muñoz <38386102+cristiantech@users.noreply.github.com>
2022-08-04 18:12:54 -0500

    Add practica al formulario

---
+++ 05-semana/practica-5.2/formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="resoluciom.php" method="post">
+    <label for="">Numero 1</label>
+    <input type="text" name="numero1" id="">
+    <label for="">Numero 2</label>
+    <input type="text" name="numero2" id="">
+    <label for="">Numero 3</label>
+    <input type="text" name="numero3" id="">
+    <input type="submit" value="Enviar">
+</form>
+    
+</body>
+</html>
